a4d7c0635d94fb4715c3ee8880f8979f92665d4b
kemysgesualdo <kemillygesualdogarcia2159@gmail.com>
2025-05-19 14:57:52 -0300

    aula 13

---
+++ aula_13/atividade clinica.sql
@@ -0,0 +1,18 @@
+use clinica_medica;
+
+show tables;
+
+desc agendamentos;
+
+select * from agendamentos;
+
+insert into agendamentos (paciente_id, medico_id, data_hora, tipo_consulta,status) VALUES
+(2,1,"2025-05-19 09:00", "chekup geral","consulta realizada"),
+(4,3,"2025-05-26 16:00", "diagnóstico de dor abdominal", "consulta agendada"),
+(2,3, "2025-06-02 14:00", "diagnóstico de dor de cabeça", "consulta agendada"),
+(5,2, "2025-05-19 11:00", "retorno com resultado de exames", "consulta realizada");
+
+#LISTAR A QUANTIDADE DE CONSULTAS POR MÉDICO
+SELECT medicos.nome, count(agendamentos.id) 
+from medicos join agendamentos
+on medicos.id = agendamentos.medico_id GROUP BY medicos.nome 
